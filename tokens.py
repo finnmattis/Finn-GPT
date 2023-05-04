c9781334065b6c338764d65ed26c7d92b66de73f
Finn Mattis <planto73@harkenpretty.com>
2023-05-03 20:03:43 -0400

    moved Tokenizer to class

--- tokens.py
+++ tokens.py
@@ -1,79 +1,72 @@
 import pandas as pd
 
-def get_most_frequent_pair(tokens):
-    pairs = {}
-    for char1, char2 in zip(tokens, tokens[1:]):
-        pair = (char1, char2)
-        pairs[pair] = pairs.get(pair, 0) + 1
+class Tokenizer:
+    def __init__(self):
+        self.merges = []
 
-    return max(pairs, key=pairs.get) if pairs else None
+    def __get_most_frequent_pair(self, tokens):
+        pairs = {}
+        for char1, char2 in zip(tokens, tokens[1:]):
+            pair = (char1, char2)
+            pairs[pair] = pairs.get(pair, 0) + 1
 
-def bpe(texts, num_merges=1000):
-    tokens_list = [list(text) for text in texts]
-    tokens = [token for tokens_sublist in tokens_list for token in tokens_sublist]
-    vocab = {token: 1 for token in tokens}
-    merges = []
+        return max(pairs, key=pairs.get) if pairs else None
 
-    for _ in range(num_merges):
-        pair = get_most_frequent_pair(tokens)
-        if not pair:
-            break
+    def train(self, texts, num_merges=1000):
+        tokens_list = [["<start>"] + list(text) + ["<end>"] for text in texts]
+        tokens = [token for tokens_sublist in tokens_list for token in tokens_sublist]
+        vocab = {token: 1 for token in tokens}
 
-        token_new = "".join(pair)
-        merges.append(pair)
-        
-        updated_tokens = []
-        skip_next = False
-        for i in range(len(tokens) - 1):
-            if skip_next:
-                skip_next = False
-                continue
+        for _ in range(num_merges):
+            pair = self.__get_most_frequent_pair(tokens)
+            if not pair:
+                break
 
-            if tokens[i] == pair[0] and tokens[i + 1] == pair[1]:
-                updated_tokens.append(token_new)
-                skip_next = True
-            else:
-                updated_tokens.append(tokens[i])
+            token_new = "".join(pair)
+            self.merges.append(pair)
+            
+            updated_tokens = []
+            skip_next = False
+            for i in range(len(tokens) - 1):
+                if skip_next:
+                    skip_next = False
+                    continue
 
-        if not skip_next:
-            updated_tokens.append(tokens[-1])
+                if tokens[i] == pair[0] and tokens[i + 1] == pair[1]:
+                    updated_tokens.append(token_new)
+                    skip_next = True
+                else:
+                    updated_tokens.append(tokens[i])
 
-        tokens = updated_tokens
-        vocab[token_new] = 1
+            if not skip_next:
+                updated_tokens.append(tokens[-1])
 
-    return tokens, vocab, merges
+            tokens = updated_tokens
+            vocab[token_new] = 1
+        return len(vocab)
 
-def encode(text, merges):
-    tokens = list(text)
+    def encode(self, text):
+        tokens = list(text)
 
-    for merge in merges:
-        new_tokens = []
-        i = 0
-        while i < len(tokens):
-            token = tokens[i]
-            if i < len(tokens) - 1 and (token, tokens[i + 1]) in merges:
-                new_tokens.append("".join((token, tokens[i + 1])))
-                i += 2
-            else:
-                new_tokens.append(token)
-                i += 1
-        tokens = new_tokens
+        for merge in self.merges:
+            new_tokens = []
+            i = 0
+            while i < len(tokens):
+                token = tokens[i]
+                if i < len(tokens) - 1 and (token, tokens[i + 1]) in self.merges:
+                    new_tokens.append("".join((token, tokens[i + 1])))
+                    i += 2
+                else:
+                    new_tokens.append(token)
+                    i += 1
+            tokens = new_tokens
 
-    return tokens
+        return tokens
 
-
-
-# text = "This is a sample text for BPE encoding."
-# tokens, vocab, merges = bpe(text, num_merges=10)
-# print("Encoded Tokens:", tokens)
-# print("Vocab:", vocab)
-#
-# novel_text = "This is a sample text for BPE encoding."
-# encoded_novel_text = encode(novel_text, merges)
-# print("Encoded Novel Text:", encoded_novel_text)
+mytok = Tokenizer()
 
 df = pd.read_json("data.json")
-tokens, vocab, merges = bpe(list(df["question"]), 100)
-print(vocab)
-print("\n\n\n")
-print(merges)
+mytok.train(list(df["question"]), 100)
+
+novel_text = "Who died today?"
+print(mytok.encode(novel_text))
