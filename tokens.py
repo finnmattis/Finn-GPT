468f0b037eeb1efed0f1348f109ffb8a6d1a097b
Finn Mattis <planto73@harkenpretty.com>
2023-05-03 20:12:40 -0400

    Finished tokenizer class

--- tokens.py
+++ tokens.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 class Tokenizer:
     def __init__(self):
-        self.merges = []
+        self.__trained = False
+        self.__merges = []
+        self.vocab = {}
 
     def __get_most_frequent_pair(self, tokens):
         pairs = {}
@@ -13,9 +15,12 @@ class Tokenizer:
         return max(pairs, key=pairs.get) if pairs else None
 
     def train(self, texts, num_merges=1000):
+        if self.__trained:
+            raise RuntimeError("Already trained!")
+
         tokens_list = [["<start>"] + list(text) + ["<end>"] for text in texts]
         tokens = [token for tokens_sublist in tokens_list for token in tokens_sublist]
-        vocab = {token: 1 for token in tokens}
+        self.vocab = {token: 1 for token in tokens}
 
         for _ in range(num_merges):
             pair = self.__get_most_frequent_pair(tokens)
@@ -23,37 +28,38 @@ class Tokenizer:
                 break
 
             token_new = "".join(pair)
-            self.merges.append(pair)
+            self.__merges.append(pair)
             
             updated_tokens = []
             skip_next = False
-            for i in range(len(tokens) - 1):
+            for token1, token2 in zip(tokens, tokens[1:]):
                 if skip_next:
                     skip_next = False
                     continue
 
-                if tokens[i] == pair[0] and tokens[i + 1] == pair[1]:
+                if token1 == pair[0] and token2 == pair[1]:
                     updated_tokens.append(token_new)
                     skip_next = True
                 else:
-                    updated_tokens.append(tokens[i])
+                    updated_tokens.append(token1)
 
             if not skip_next:
                 updated_tokens.append(tokens[-1])
 
             tokens = updated_tokens
-            vocab[token_new] = 1
-        return len(vocab)
+            self.vocab[token_new] = 1
+
+        self.__trained = True
 
     def encode(self, text):
         tokens = list(text)
 
-        for merge in self.merges:
+        for merge in self.__merges:
             new_tokens = []
             i = 0
             while i < len(tokens):
                 token = tokens[i]
-                if i < len(tokens) - 1 and (token, tokens[i + 1]) in self.merges:
+                if i < len(tokens) - 1 and (token, tokens[i + 1]) in self.__merges:
                     new_tokens.append("".join((token, tokens[i + 1])))
                     i += 2
                 else:
